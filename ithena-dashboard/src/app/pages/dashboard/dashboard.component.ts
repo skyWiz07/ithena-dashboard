c0185e816e7dbf58f03234f3976581e1db552849
skyWiz <76843441+skyWiz07@users.noreply.github.com>
2024-05-15 17:21:44 +0530

    dynamic chart data

--- ithena-dashboard/src/app/pages/dashboard/dashboard.component.ts
+++ ithena-dashboard/src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MrrService } from 'src/app/services/mrr.service';
+import { MtrService } from 'src/app/services/mtr.service';
+import { ViewsService } from 'src/app/services/views.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
-export class DashboardComponent {}
+export class DashboardComponent implements OnInit {
+  public barChartDataset: any = [];
+  public bubbleChartDataset: any = [];
+  public doughnutChartDataset: any = [];
+
+  constructor(
+    private mtrService: MtrService,
+    private mrrService: MrrService,
+    private viewsService: ViewsService
+  ) {}
+
+  ngOnInit() {
+    this.loadDashboardData();
+  }
+
+  private loadDashboardData() {
+    //TODO: send some date params
+    this.getMTRData();
+    this.getMRRDatabyCountry();
+    this.getPageViews();
+  }
+
+  public dateRangeChangeHandler() {
+    this.loadDashboardData();
+  }
+
+  private getMRRData() {}
+
+  private getMTRData(params?: any) {
+    this.mtrService.getMTRData(params).subscribe((data) => {
+      this.barChartDataset = data;
+    });
+  }
+
+  private getPageViews(params?: any) {
+    this.viewsService.getPageViewsData(params).subscribe((data) => {
+      this.doughnutChartDataset = data;
+    });
+  }
+
+  private getMRRDatabyCountry(params?: any) {
+    this.mrrService.getMRRStatsByCountryData(params).subscribe((data) => {
+      this.bubbleChartDataset = data;
+    });
+  }
+}
